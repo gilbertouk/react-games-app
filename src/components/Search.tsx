2b01cfc872aae6799bc6d9d2b534e01dae95b545
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-04-25 00:07:51 +0100

    feat: add Search component for game search functionality

---
+++ src/components/Search.tsx
@@ -0,0 +1,25 @@
+import { Dispatch, SetStateAction } from "react";
+
+interface SearchProps {
+  searchTerm: string;
+  setSearchTerm: Dispatch<SetStateAction<string>>;
+}
+
+const Search = ({ searchTerm, setSearchTerm }: SearchProps) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search through thousands of games..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
